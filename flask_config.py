fbdae4c81c0aaa5b6abde3571073b78d50060b68
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-02-14 14:28:05 +0000

    Update environment to use secret_key

--- flask_config.py
+++ flask_config.py
@@ -1,7 +1,12 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     def __init__(self):
         """Base configuration variables."""
-        self.SECRET_KEY = "secret-key"
+        self.SECRET_KEY = os.environ.get("SECRET_KEY")
+        if not self.SECRET_KEY:
+            raise("Secret key missing. Something is wrong")
