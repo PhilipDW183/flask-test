a9b4cd12231ef24b9faa67efd78a375f5a494837
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-02-14 15:19:05 +0000

    Refactor into routes

--- core/app.py
+++ core/app.py
@@ -9,37 +9,7 @@ def create_app():
     app.config.from_object(Config())
 
 
-    @app.route('/')
-    def index():
-    	items = get_items()
-    	return render_template("index.html", items=items)
-
-
-    @app.route('/create_new_task', methods=["POST"])
-    def create_new_task():
-        new_item = request.form.get('todo')
-        add_item(new_item)
-        return redirect(url_for("index"))
-
-    @app.route('/doing_item/<id>')
-    def doing_item(id):
-        update_item(id, "Doing")
-        return redirect(url_for("index"))
-
-    @app.route('/to_do_item/<id>')
-    def to_do_item(id):
-        update_item(id, "Not Started")
-        return redirect(url_for("index"))
-
-    @app.route('/complete_item/<id>')
-    def complete_item(id):
-        update_item(id, "Completed")
-        return redirect(url_for("index"))
-
-
-    @app.route('/delete_item/<id>')
-    def delete_item(id):
-        remove_item(id)
-        return redirect(url_for("index"))
+    from core.main import home as home_bp
+    app.register_blueprint(home_bp)
 
     return app
