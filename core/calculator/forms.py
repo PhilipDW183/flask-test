1f717f43e0e4cbb2e93eec79b545eda3328a996d
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-02-16 14:51:51 +0000

    CALCULATOR-use flask forms to take input

---
+++ core/calculator/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import (SelectField,  DecimalField, SubmitField)
+from wtforms.validators import InputRequired
+
+class CalculatorForm(FlaskForm):
+    input1 = DecimalField("Input 1", validators = [InputRequired()])
+    input2 = DecimalField("Input 2", validators = [InputRequired()])
+    operation = SelectField("Operation", choices=[("+", "Addition"), 
+        ("-", "Subtraction"),
+        ("/", "Division"),
+        ("*", "Multiplication")],
+        validators = [InputRequired()])
+    submit = SubmitField("Calculate")
--- core/calculator/routes.py
+++ core/calculator/routes.py
@@ -1,55 +1,47 @@
 from core.calculator import calculator
 from flask import render_template, url_for, redirect, request
+from core.calculator.forms import CalculatorForm
 
 base_path = "/calculator"
 
-@calculator.route(base_path)
+@calculator.route(base_path, methods=("GET", "POST"))
 def calculator_index():
-    return render_template("calculator.html")
+    form = CalculatorForm()
+    if form.validate_on_submit():
+            first_input = form.input1.data
+            second_input = form.input2.data
+            operation = form.operation.data
 
-@calculator.route(f"{base_path}/calculate", methods=["POST"])
-def calculate():
-    first_input = request.form.get("input1")
-    second_input = request.form.get("input2")
-    operation = request.form.get("operation")
+            try:
+                input1 = float(first_input)
+                input2 = float(second_input)
 
-    try:
-        input1 = float(first_input)
-        input2 = float(second_input)
-
-        if operation == "+":
-            result = input1 + input2
-        elif operation == "-":
-            result = input1 - input2
-        elif operation == "*":
-            result = input1 * input2
-        else:
-            if (input1==0 and input2 ==0):
-                result = 0
-            else:
-                result = input1 / input2
-
-        return render_template("calculator.html",
-            input1 = input1,
-            input2 = input2,
-            operation = operation,
-            result = result,
-            calculation_success=True)
-    except ZeroDivisionError:
-        return render_template("calculator.html",
-            input1 = input1,
-            input2 = input2,
-            operation = operation,
-            result = "Bad input",
-            calculation_success=False,
-            error="You cannot divide by zero")
-    except ValueError:
-        return render_template("calculator.html",
-            input1 = first_input,
-            input2 = second_input,
-            operation = operation,
-            result = "Bad input",
-            calculation_success=False,
-            error="Operation cannot be performed with input provided")
-
+                if operation == "+":
+                    result = input1 + input2
+                elif operation == "-":
+                    result = input1 - input2
+                elif operation == "*":
+                    result = input1 * input2
+                else:
+                    if (input1==0 and input2 ==0):
+                        result = 0
+                    else:
+                        result = input1 / input2
+                return render_template("calculator.html",
+                    form = form,
+                    input1 = input1,
+                    input2 = input2,
+                    operation = operation,
+                    result = result,
+                    calculation_success=True)
+            except ZeroDivisionError:
+                return render_template("calculator.html",
+                    form=form,
+                    input1 = input1,
+                    input2 = input2,
+                    operation = operation,
+                    result = "Bad input",
+                    calculation_success=False,
+                    error="You cannot divide by zero")
+    return render_template("calculator.html", form=form)
 
