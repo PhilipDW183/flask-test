4e37d9acea8f7c0c80cb6d09c295e1076f8fe691
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2023-02-16 15:12:02 +0000

    CALCULATOR-refactor to use operator module

--- core/calculator/routes.py
+++ core/calculator/routes.py
@@ -1,8 +1,19 @@
 from core.calculator import calculator
-from flask import render_template, url_for, redirect, request
+from flask import render_template
 from core.calculator.forms import CalculatorForm
+import operator
 
 base_path = "/calculator"
+
+ops = {
+    '+' : operator.add,
+    '-' : operator.sub,
+    '*' : operator.mul,
+    '/' : operator.truediv,  # use operator.div for Python 2
+    '%' : operator.mod,
+    '^' : operator.xor,
+}
+
 
 @calculator.route(base_path, methods=("GET", "POST"))
 def calculator_index():
@@ -16,17 +27,11 @@ def calculator_index():
                 input1 = float(first_input)
                 input2 = float(second_input)
 
-                if operation == "+":
-                    result = input1 + input2
-                elif operation == "-":
-                    result = input1 - input2
-                elif operation == "*":
-                    result = input1 * input2
+                if (input1==0 and input2 ==0 and operation == "/"):
+                    result = 0
                 else:
-                    if (input1==0 and input2 ==0):
-                        result = 0
-                    else:
-                        result = input1 / input2
+                    result = ops[operation](input1, input2)
+
                 return render_template("calculator.html",
                     form = form,
                     input1 = input1,
